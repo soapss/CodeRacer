ec7ca510c8b7982c07366ff8258f586243bc964a
soapss <76599851+soapss@users.noreply.github.com>
2023-10-10 14:51:45 -0700

    setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.use(cors()); 
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+    const [message, setMessage] = useState('');
+
+    const handleButtonClick = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/api/data');
+            if (!response.ok) {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+            const data = await response.json();
+            setMessage(data.message);
+        } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        }
+    };
+
+    return (
+    <div>
+        <button onClick={handleButtonClick}>Get Message</button>
+        {message && <div>{message}</div>}
+    </div>
+    );
+};
+
+export default Home;
